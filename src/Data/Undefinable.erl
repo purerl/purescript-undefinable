e97e70a0c58d5c364a53f6b27526bdfd8cb4aa21
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2019-03-26 17:38:38 +0000

    Are you kidding me

--- src/Data/Undefinable.erl
+++ src/Data/Undefinable.erl
@@ -3,7 +3,7 @@
 
 undefined() -> undefined.
 
-undefinable() -> fun (A, R, _F) when A =:= null -> R;
+undefinable() -> fun (A, R, _F) when A =:= undefined -> R;
                      (A, _R, F) -> F(A)
                  end.
 
